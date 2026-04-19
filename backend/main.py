d44e60074a8973beabb778ce2c99d48365c17d70
Krutiktikam <krutiktikam7@gmail.com>
2026-04-19 18:10:08 +0530

    fix: explicit CORS origins to support withCredentials in production

--- backend/main.py
+++ backend/main.py
@@ -38,7 +38,8 @@ if frontend_url:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
